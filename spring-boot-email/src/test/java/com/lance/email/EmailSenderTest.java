e7cd6c71bc5c32df94502e115718801983698ea4
lantlot <lantlot@gmail.com>
2018-02-22 17:58:02 +0800

    Upgrade Spring Boot Version to 1.5.10.RELEASE ; Fix compile error

--- spring-boot-email/src/test/java/com/lance/email/EmailSenderTest.java
+++ spring-boot-email/src/test/java/com/lance/email/EmailSenderTest.java
@@ -5,11 +5,11 @@ import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes=SimpleApplication.class)
+@SpringBootTest(classes=SimpleApplication.class)
 public class EmailSenderTest {
 	private Logger logger = LogManager.getLogger(getClass());
 	@Autowired
